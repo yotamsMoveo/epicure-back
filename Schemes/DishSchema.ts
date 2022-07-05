0b5da523d838dfd4d30fe70032e17edf269cfaa6
yotamsMoveo <yotams@moveo.co.il>
2022-07-05 11:51:04 +0300

    set schema

--- Schemes/DishSchema.ts
+++ Schemes/DishSchema.ts
@@ -5,7 +5,7 @@ export interface IDish {
   name: string;
   image: string;
   description: string;
-  type: string;
+  type: string[];
   price:number;
   dish_time:string;
   restaurant:IRestaurant;
@@ -17,7 +17,7 @@ const DishSchema = new Schema<IDish>(
     name: { type: String ,unique:false},
     image: { type: String,unique:false},
     description: { type:String ,unique:false},
-    type: { type: String ,unique:false},
+    type: [{ type: String ,unique:false}],
     price: { type:Number ,unique:false},
     dish_time: { type: String,unique:false},
     restaurant: { type: mongoose.Schema.Types.ObjectId, ref: "Restaurants",unique:false},
